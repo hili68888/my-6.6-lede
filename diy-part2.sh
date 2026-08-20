24ec41ccf1d1043bb2c8d5b58af78f163dd4a279
hili68888 <99248065+hili68888@users.noreply.github.com>
2026-08-20 12:24:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,10 +4,17 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# TTYD 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 1. 【通用强改 6.6】不管当前默认是 6.12 还是其他任何版本，一律强制清洗并锁死为 6.6
 if [ -f "target/linux/qualcommax/Makefile" ]; then
     sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.6/g' target/linux/qualcommax/Makefile
+fi
+
+# 1.5 【核心报错修复】既然编译纯有线固件，直接强行删除引发冲突的高通无线内核补丁
+if [ -f "target/linux/qualcommax/patches-6.6/0113-remoteproc-qcom-Add-secure-PIL-support.patch" ]; then
+    rm -f target/linux/qualcommax/patches-6.6/0113-remoteproc-qcom-Add-secure-PIL-support.patch
 fi
 
 # 2. 自动将基础组件中的 dnsmasq 替换为 dnsmasq-full，从源头规避包冲突
@@ -42,6 +49,10 @@ CONFIG_PACKAGE_luci-i18n-fileassistant-zh-cn=y
 CONFIG_PACKAGE_kmod-ath11k=n
 CONFIG_PACKAGE_kmod-ath11k-ahb=n
 CONFIG_PACKAGE_wpad-openssl=n
+
+# 针对无 USB 接口硬件，彻底关闭上层挂载与存储组件
+CONFIG_PACKAGE_kmod-usb-storage=n
+CONFIG_PACKAGE_kmod-usb-core=n
 
 # ==================== 【终极网络代理依赖全家桶（轻量化纯净版）】 ====================
 # 核心底层控制与传输
