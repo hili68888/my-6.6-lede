a758e4f484f3506f3b3b52fb32579cdf2cf12137
hili68888 <99248065+hili68888@users.noreply.github.com>
2026-08-20 14:50:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,11 +4,11 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 1. 【优雅切换 6.6】利用大雕和 LibWrt 源码原生的 KERNEL_TESTING 机制完美切换至 6.6
-# 这样系统在打补丁时会使用最安全的官方原生方案，不会因为强行覆盖导致 drivers/remoteproc 报错
 if [ -f "target/linux/qualcommax/Makefile" ]; then
     sed -i 's/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)/g' target/linux/qualcommax/Makefile
 fi
@@ -20,3 +20,15 @@ fi
 
 # 2. 自动将基础组件中的 dnsmasq 替换为 dnsmasq-full，从源头规避包冲突
 find include/ target/ -name "Makefile" -o -name "*.mk" | xargs sed -i 's/\bdnsmasq\b/dnsmasq-full/g' 2>/dev/null
+
+
+# =====================================================================
+# 3. 【核心修复】强行向 .config 追加锁死高通 qualcommax 架构基础参数
+# 防止被系统自带的 make defconfig 识别为“非法残缺配置”而重置为默认的 x86-64
+# =====================================================================
+echo 'CONFIG_TARGET_qualcommax=y' >> .config
+echo 'CONFIG_TARGET_qualcommax_ipq60xx=y' >> .config
+echo 'CONFIG_TARGET_qualcommax_ipq60xx_DEVICE_cmiot_ax18=y' >> .config
+echo 'CONFIG_TARGET_BOARD="qualcommax"' >> .config
+echo 'CONFIG_TARGET_SUBTARGET="ipq60xx"' >> .config
+echo 'CONFIG_TARGET_ARCH_PACKAGES="aarch64_cortex-a53"' >> .config
