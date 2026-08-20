104008a71e4956b4f12e4553e34dba2593d624df
hili68888 <99248065+hili68888@users.noreply.github.com>
2026-08-20 08:50:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # 2. 单独克隆 fileassistant 源码包到 package 目录下
-git clone https://github.com/kenzok78/luci-app-fileassistant.git
+echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
